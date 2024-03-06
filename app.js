3fa048d44ec1cddaa85206aaa8aa9c159eb2184b
Jesus0204 <a01705442@tec.mx>
2024-03-06 14:29:21 -0600

    Agregar paquetes necesarios a app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+// Para usar express en vez de http
+const express = require('express');
+// Inicia la app usuando a express
+const app = express();
+
+// Configuras a EJS como motor de templates con express
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const path = require('path');
+
+// Para que se puede usar cookie parser de forma mas facil
+const cookieParser = require('cookie-parser')
+app.use(cookieParser('Un secreto'))
+
+// Para usar las sesiones
+const session = require('express-session');
+
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste',
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
+// La aplicacion va a tener acceso a todo lo que esta en public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Manipular facil los datos de las peticiones
+const bodyParser = require('body-parser');
+
+// Configura bodyparser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
