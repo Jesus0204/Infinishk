67f4fbd1c6f6ccff719ea0aab81206d60091aa36
alexmushi <A01705550@tec.mx>
2024-03-06 23:32:53 -0600

    Esqueleto de A2 agregado, correción de selección de A3

--- controllers/alumnos.controller.js
+++ controllers/alumnos.controller.js
@@ -1,6 +1,9 @@
 exports.get_A1 = (request, response, next) => {
     response.render('A1');
 };
+exports.get_A2 = (request, response, next) => {
+    response.render('A2');
+};
 exports.get_A3 = (request, response, next) => {
     response.render('A3');
 };
--- routes/alumnos.routes.js
+++ routes/alumnos.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const alumnosController = require('../controllers/alumnos.controller');
 
 router.get('/A1', alumnosController.get_A1);
+router.get('/A2', alumnosController.get_A2);
 router.get('/A3', alumnosController.get_A3);
 
 // Con esta linea se permite que se exporte en el principal
