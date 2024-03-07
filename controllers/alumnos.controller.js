9ea969ca76be992985d730c7e12ac60b1c1a6453
alexmushi <A01705550@tec.mx>
2024-03-06 23:00:12 -0600

    ruta para A1

--- controllers/alumnos.controller.js
+++ controllers/alumnos.controller.js
@@ -1,3 +1,6 @@
+exports.get_A1 = (request, response, next) => {
+    response.render('A1');
+};
 exports.get_A3 = (request, response, next) => {
     response.render('A3');
 };
--- routes/alumnos.routes.js
+++ routes/alumnos.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const alumnosController = require('../controllers/alumnos.controller');
 
+router.get('/A1', alumnosController.get_A1);
 router.get('/A3', alumnosController.get_A3);
 
 // Con esta linea se permite que se exporte en el principal
