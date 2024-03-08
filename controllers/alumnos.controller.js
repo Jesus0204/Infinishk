d8a8150ab28c0717ab173a72604d51f39dc166ab
Mike Barrón <mikebarron@Mikes-MacBook-Pro.local>
2024-03-07 20:53:19 -0600

    Ruta realizar pago

--- controllers/alumnos.controller.js
+++ controllers/alumnos.controller.js
@@ -4,8 +4,8 @@ exports.get_inicio = (request, response, next) => {
 exports.get_horario = (request, response, next) => {
     response.render('horario');
 };
-exports.get_A3 = (request, response, next) => {
-    response.render('A3');
+exports.get_realizar_pago = (request, response, next) => {
+    response.render('realizar_pago');
 };
 exports.get_estado_de_cuenta = (request, response, next) => {
     response.render('estado_de_cuenta');
--- routes/alumnos.routes.js
+++ routes/alumnos.routes.js
@@ -8,7 +8,7 @@ const alumnosController = require('../controllers/alumnos.controller');
 
 router.get('/inicio', alumnosController.get_inicio);
 router.get('/horario', alumnosController.get_horario);
-router.get('/A3', alumnosController.get_A3);
+router.get('/realizar_pago', alumnosController.get_realizar_pago);
 router.get('/estado_de_cuenta', alumnosController.get_estado_de_cuenta);
 router.get('/test', alumnosController.get_test);
 
