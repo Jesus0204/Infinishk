fd5b80b89f42a389e34f1bdef5181f47ed391de3
alexmushi <A01705550@tec.mx>
2024-03-07 23:07:44 -0600

    Rutas corregidas de visualizador, V3 agregado

--- controllers/visualizador.controller.js
+++ controllers/visualizador.controller.js
@@ -1,3 +1,12 @@
 exports.get_ruta = (request, response, next) => {
     response.render('');
 };
+exports.get_V1 = (request, response, next) => {
+    response.render('V1');
+};
+exports.get_V2 = (request, response, next) => {
+    response.render('V2');
+};
+exports.get_V3 = (request, response, next) => {
+    response.render('V3');
+};
--- routes/visualizador.routes.js
+++ routes/visualizador.routes.js
@@ -7,6 +7,9 @@ const visualizadorController = require('../controllers/visualizador.controller')
 
 
 router.get('/ruta', visualizadorController.get_ruta);
+router.get('/V1', visualizadorController.get_V1);
+router.get('/V2', visualizadorController.get_V2);
+router.get('/V3', visualizadorController.get_V3);
 
 // Con esta linea se permite que se exporte en el principal
 module.exports = router;
