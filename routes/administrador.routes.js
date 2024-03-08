102b69e63152c2eb609bf38137b90dfc4a048ddb
Mike Barrón <mikebarron@Mikes-MacBook-Pro.local>
2024-03-07 22:48:27 -0600

    Correccion detalle admin routes

--- routes/administrador.routes.js
+++ routes/administrador.routes.js
@@ -6,15 +6,15 @@ const router = express.Router();
 const adminController = require('../controllers/administrador.controller');
 
 
-router.get('/S1', adminController.S1);
-router.get('/S2', adminController.S2);
-router.get('/S3', adminController.S3);
-router.get('/S4', adminController.S4);
-router.get('/S5', adminController.S5);
-router.get('/S6', adminController.S6);
-router.get('/S7', adminController.S7);
-router.get('/S8', adminController.S8);
-router.get('/S9', adminController.S9);
+router.get('/S1', adminController.get_S1);
+router.get('/S2', adminController.get_S2);
+router.get('/S3', adminController.get_S3);
+router.get('/S4', adminController.get_S4);
+router.get('/S5', adminController.get_S5);
+router.get('/S6', adminController.get_S6);
+router.get('/S7', adminController.get_S7);
+router.get('/S8', adminController.get_S8);
+router.get('/S9', adminController.get_S9);
 
 // Con esta linea se permite que se exporte en el principal
 module.exports = router;
