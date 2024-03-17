34428d59cf918c95c82a4db728946543f871b9ec
Zathiald <A01705403.com>
2024-03-17 12:33:37 -0600

    Agregar controladores a rutas

---
+++ routes/session.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+// Ahora en vez de usar app, se usa el router de express
+const router = express.Router();
+
+const sessionController = require('../controllers/sesion.controller');
