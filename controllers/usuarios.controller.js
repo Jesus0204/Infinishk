741ccee4c47b375b3f5b22e1c6198e0f52adb8df
Jesus0204 <a01705442@tec.mx>
2024-03-30 15:14:06 -0600

    Avance inicial ejs

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -8,26 +8,31 @@ exports.get_alumnos_atrasados = (request, response, next) => {
     // Primero sacas las matriculas de alumnos que estan atrasados
     Deuda.fetchNoPagados()
         .then(([alumnos_atrasados, fieldData]) => {
-            
-            let deuda = [];
-            // Para cada alumno atrasado sacas todos los datos
-            for (let alumno of alumnos_atrasados) {
-                Deuda.fetchDeuda(alumno.matricula)
-                    .then(([deuda_alumno, fieldData]) => {
-                        deuda.push(deuda_alumno);
-                        console.log(deuda);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    })
-            }
-            // Pasas a plantilla deudas de alumnos que tienen pago atrasado
-            response.render('usuarios/alumnos_atrasados', {
-                deudas_atrasadas: deuda
-            });
+
+            Deuda.fetchDeuda(alumnos_atrasados[0].matricula)
+            .then(([deuda_alumno, fieldData]) => {
+                 // Pasas a plantilla deudas de alumnos que tienen pago atrasado
+                 response.render('usuarios/alumnos_atrasados', {
+                     deudas_atrasadas: deuda_alumno
+                 });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            // let deuda = [];
+            // // Para cada alumno atrasado sacas todos los datos
+            // for (let alumno of alumnos_atrasados) {
+            //     Deuda.fetchDeuda(alumno.matricula)
+            //         .then(([deuda_alumno, fieldData]) => {
+            //             deuda.push(deuda_alumno);
+            //             console.log(deuda);
+            //         })
+            //         .catch((error) => {
+            //             console.log(error);
+            //         })
+            // }
         })
         .catch((error) => {
             console.log(error);
         });
-
 };
