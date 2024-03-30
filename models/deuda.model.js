f7a774efb9186dc49a5472aef802e95c50ed40ba
Jesus0204 <a01705442@tec.mx>
2024-03-29 19:33:41 -0600

    Agregar base de modelo deuda a develop

---
+++ models/deuda.model.js
@@ -0,0 +1,9 @@
+const db = require('../util/database');
+
+module.exports = class Deuda {
+    // Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    constructor() {
+
+    }
+    
+}
