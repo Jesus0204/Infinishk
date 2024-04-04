7a8ab0b719250829394e416e7f2342913b9dc2d9
Jesus0204 <a01705442@tec.mx>
2024-04-03 21:26:21 -0600

    Codigo base login

--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -14,5 +14,45 @@ exports.get_login = (request, response, next) => {
 };
 
 exports.post_login = (request, response, next) => {
-
+    Usuario.fetchOne(request.body.username)
+        .then(([users, fieldData]) => {
+            if (users.length == 1) {
+                // users[0] contiene el objeto de la respuesta de la consulta
+                const user = users[0];
+                // Ya que verificamos que el usuario existe en la base de datos
+                bcrypt.compare(request.body.password, user.password)
+                    .then(doMatch => {
+                        // Si la promesa es verdadero, entonces inicias sesion en la pagina
+                        if (doMatch) {
+                            Usuario.getPermisos(user.username)
+                                .then(([permisos, fieldData]) => {
+                                    request.session.isLoggedIn = true;
+                                    request.session.permisos = permisos;
+                                    console.log(request.session.permisos);
+                                    request.session.username = user.username;
+                                    return request.session.save(err => {
+                                        response.redirect('/mensajes');
+                                    })
+                                })
+                                .catch((error) => {
+                                    console.log(error)
+                                });
+                            // Por si los passwords no hacen match
+                        } else {
+                            request.session.error = 'El usuario y/o contraseña con incorrectos.';
+                            return response.redirect('/users/login');
+                        }
+                        // Por si hay un error en la libreria o algo
+                    }).catch(err => {
+                        response.redirect('/users/login');
+                    });
+                // Por si el usuario no existe en la base de datos
+            } else {
+                request.session.error = 'El usuario y/o contraseña con incorrectos.';
+                response.redirect('/users/login');
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 };
