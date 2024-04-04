51183c37244f7544f2d6308def542d63eef47148
Jesus0204 <a01705442@tec.mx>
2024-04-03 21:01:37 -0600

    Agregar cosas necesarias para iniciar sesion

--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -4,5 +4,10 @@ const bcrypt = require('bcryptjs');
 exports.get_login = (request, response, next) => {
     const error = request.session.error || '';
     request.session.error = '';
-    response.render('login');
+    response.render('login', {
+        username: request.session.username || '',
+        error: error,
+        csrfToken: request.csrfToken(),
+        permisos: request.session.permisos || [],
+    });
 };
