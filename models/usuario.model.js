21f7d153c6f0c610bc8ef1dbac30f7d2d5ee6f12
alexmushi <A01705550@tec.mx>
2024-04-03 20:48:51 -0600

    Class Usuario

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,6 +1,6 @@
 const db = require('../util/database');
 
-module.exports = class Cursa{
+module.exports = class Usuario{
     // Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(mi_IDUsuario,mi_Contrasena,mi_UsuarioActivo){
         this.IDUsuario = mi_IDUsuario;
