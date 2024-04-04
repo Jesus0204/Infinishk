dea13e387c4ebb4d04c4973a61f8e219f7a8081e
Jesus0204 <a01705442@tec.mx>
2024-04-03 21:06:56 -0600

    Corregir error

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -3,6 +3,8 @@ const express = require('express');
 // Ahora en vez de usar app, se usa el router de express
 const router = express.Router();
 
-const sessionController = require('../controllers/sesion.controller');
+const sessionController = require('../controllers/session.controller');
 
 router.get('/login', sessionController.get_login);
+
+module.exports = router;
