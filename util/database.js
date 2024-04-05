b741a1bf31ac0e7a9ca4113123e221066ef30062
Jesus0204 <a01705442@tec.mx>
2024-04-05 11:07:34 -0600

    Base de datos

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'VIAPAGO',
+    database: 'VIA_PAGO',
     password: 'Queretaro2017#'
 });
 
