41c829b48dce4ca4ada6cc0efd2f19a1908e2af9
Jesus0204 <a01705442@tec.mx>
2024-04-08 20:27:27 -0600

    Borrar archivos inecesarios y agregar proteger rutas diplomado]

--- routes/diplomado.routes.js
+++ routes/diplomado.routes.js
@@ -5,21 +5,20 @@ const router = express.Router();
 
 const diplomadoController = require('../controllers/diplomado.controller');
 
-router.get('/',diplomadoController.get_diplomado);
+// Incluyes el archivo para verificar si esta autenticado y los permisos
+const isAuth = require('../util/is-Auth');
+const can_ConsultarDiplomado = require('../util/privileges/can_consultar_diplomado');
+const can_RegistrarDiplomado = require('../util/privileges/admin/registros/can_registrar_Diplomado');
+const can_ModificarDiplomado = require('../util/privileges/admin/otros/can_modificar_Diplomado');
 
 router.get('/registrar_diplomado',diplomadoController.get_registrar_diplomado);
-
 router.post('/registrar_diplomado',diplomadoController.post_registrar_diplomado);
-
 router.get('/editar_diplomado',diplomadoController.get_modificar_diplomado);
-
 router.get('/check_diplomado', diplomadoController.get_check_diplomado);
-
 router.get('/autocomplete', diplomadoController.get_autocomplete);
-
 router.post('/editar_diplomado',diplomadoController.post_fetch_diplomado);
-
 router.post('/resultado_diplomado',diplomadoController.post_modificar_diplomado);
+router.get('/', diplomadoController.get_diplomado);
 
 
 module.exports = router;
