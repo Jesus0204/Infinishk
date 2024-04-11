aa7699ad88ef911beaf37fb5c665d88dfa48bf81
Zathiald <A01705403@tec.mx>
2024-04-11 15:56:03 -0600

    Agregar isAuth a las rutas

--- routes/diplomado.routes.js
+++ routes/diplomado.routes.js
@@ -11,13 +11,13 @@ const can_ConsultarDiplomado = require('../util/privileges/can_consultar_diploma
 const can_RegistrarDiplomado = require('../util/privileges/admin/registros/can_registrar_Diplomado');
 const can_ModificarDiplomado = require('../util/privileges/admin/otros/can_modificar_Diplomado');
 
-router.get('/registrar_diplomado',can_RegistrarDiplomado,diplomadoController.get_registrar_diplomado);
-router.post('/registrar_diplomado',can_RegistrarDiplomado,diplomadoController.post_registrar_diplomado);
-router.get('/editar_diplomado',can_ModificarDiplomado,diplomadoController.get_modificar_diplomado);
-router.get('/check_diplomado',can_ModificarDiplomado,diplomadoController.get_check_diplomado);
-router.get('/autocomplete',can_ModificarDiplomado,diplomadoController.get_autocomplete);
-router.post('/editar_diplomado',can_ModificarDiplomado,diplomadoController.post_fetch_diplomado);
-router.post('/resultado_diplomado',can_ModificarDiplomado,diplomadoController.post_modificar_diplomado);
+router.get('/registrar_diplomado',isAuth,can_RegistrarDiplomado,diplomadoController.get_registrar_diplomado);
+router.post('/registrar_diplomado',isAuth,can_RegistrarDiplomado,diplomadoController.post_registrar_diplomado);
+router.get('/editar_diplomado',isAuth,can_ModificarDiplomado,diplomadoController.get_modificar_diplomado);
+router.get('/check_diplomado',isAuth,can_ModificarDiplomado,diplomadoController.get_check_diplomado);
+router.get('/autocomplete',isAuth,can_ModificarDiplomado,diplomadoController.get_autocomplete);
+router.post('/editar_diplomado',isAuth,can_ModificarDiplomado,diplomadoController.post_fetch_diplomado);
+router.post('/resultado_diplomado',isAuth,can_ModificarDiplomado,diplomadoController.post_modificar_diplomado);
 router.get('/', diplomadoController.get_diplomado);
 
 module.exports = router;
