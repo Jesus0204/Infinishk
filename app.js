9ef6272a0d599b87468276a7f24168c0547e7831
Zathiald <A01705403@tec.mx>
2024-04-12 20:27:08 -0600

    Agregar controller de alumnos

--- app.js
+++ app.js
@@ -73,6 +73,9 @@ app.use('/configuracion', rutasConfiguracion);
 const rutasPago = require('./routes/pagos.routes');
 app.use('/pagos', rutasPago);
 
+const rutasAlumnos = require('./routes/alumnos.routes');
+app.use('/alumnos', rutasAlumnos);
+
 // Agregar funcion para iterar la lista del ejs, y que el codigo se vea limpio
 app.locals.contienePermiso = (permisos, casoUso) => {
 
