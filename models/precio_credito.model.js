f413e535f2562dbab396b49c1af8c165eab67fcb
A01710029 <palmadamartinez@gmail.com>
2024-04-16 00:21:05 -0600

    Corregir mensajes de error y frontend

--- models/precio_credito.model.js
+++ models/precio_credito.model.js
@@ -9,12 +9,7 @@ module.exports = class PrecioCredito {
         return db.execute(`SELECT precioPesos, fechaModificacion FROM precioCredito WHERE precioActivo = 1`);
     }
 
-    save() {
-        return db.execute(`INSERT INTO precioCredito (precioPesos, fechaModificacion, precioActivo) 
-        VALUES(?, NOW(), 1)`, [this.monto]);
-    }
-
-    static update() {
-        return db.execute(`UPDATE precioCredito SET precioActivo = 0 WHERE precioActivo = 1`);
+    static update(monto) {
+        return db.execute(`CALL InsertarPrecioCredito(?)`, [monto]);
     }
 };
