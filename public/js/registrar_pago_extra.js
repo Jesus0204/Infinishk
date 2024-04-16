d3279304fc7aa3ace0922b4da7bb3f60d4924d00
Jesus0204 <a01705442@tec.mx>
2024-04-16 09:42:52 -0600

    Mejorar seguridad con helmet

---
+++ public/js/registrar_pago_extra.js
@@ -0,0 +1,51 @@
+// Crear constantes para acceder a HTML
+const bt_Registrar = document.querySelector('#Boton_registrar');
+const motivo = document.querySelector('#motivo');
+const monto = document.querySelector('#monto');
+const ayuda_motivo = document.querySelector('#ayuda_motivo');
+const ayuda_monto_vacio = document.querySelector('#ayuda_monto_vacio');
+const ayuda_monto_negativo = document.querySelector('#ayuda_monto_negativo');
+const ayuda_monto_exponente = document.querySelector('#ayuda_monto_exponente');
+
+// Checar si hay contenido dentro del input, pata desactivar el boton
+function checar_contenido() {
+    bt_Registrar.disabled = motivo.value.length === 0 || monto.value.length === 0 || monto.value < 0 ||
+        monto.value === '0' || monto.value === '-0';
+}
+
+// Activar mensaje si el motivo no tiene input
+function mensaje_motivo() {
+    if (motivo.value.length === 0) {
+        ayuda_motivo.classList.remove('is-hidden');
+    } else {
+        ayuda_motivo.classList.add('is-hidden');
+    }
+}
+
+function mensaje_monto() {
+    if (monto.value.length === 0) {
+        ayuda_monto_vacio.classList.remove('is-hidden');
+    } else {
+        ayuda_monto_vacio.classList.add('is-hidden');
+    }
+
+    if (monto.value < 0) {
+        ayuda_monto_negativo.classList.remove('is-hidden');
+    } else if (monto.value === '0' || monto.value === '-0') {
+        ayuda_monto_negativo.classList.remove('is-hidden');
+    } else {
+        ayuda_monto_negativo.classList.add('is-hidden');
+    }
+
+    if (monto.value.includes('e') || monto.value.includes('E')) {
+        bt_Registrar.disabled = true;
+        ayuda_monto_exponente.classList.remove('is-hidden');
+    } else {
+        ayuda_monto_exponente.classList.add('is-hidden');
+    }
+}
+// Detectar si el usuario maneja input y llamar las funciones anteriores
+motivo.addEventListener('input', checar_contenido);
+monto.addEventListener('input', checar_contenido);
+motivo.addEventListener('input', mensaje_motivo);
+monto.addEventListener('input', mensaje_monto);
