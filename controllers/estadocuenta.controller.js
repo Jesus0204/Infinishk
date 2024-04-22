9e7bbbc39cb5142240f13b1178dd38f8c88d3f0e
alexmushi <A01705550@tec.mx>
2024-04-22 12:06:58 -0600

    Primera consulta para estado de cuenta

--- controllers/estadocuenta.controller.js
+++ controllers/estadocuenta.controller.js
@@ -2,13 +2,24 @@ const Deuda = require('../models/deuda.model');
 const Pago = require('../models/pago.model');
 const Alumno = require('../models/alumno.model');
 const Periodo = require('../models/periodo.model');
+const EstudianteProfesional = require('../models/estudianteprofesional.model');
 
 
-exports.get_estado_cuenta = (request,response,next) => {
-    response.render('estadocuenta/estado_cuenta', {
-        username: request.session.username || '',
-        permisos: request.session.permisos || [],
-        rol: request.session.rol || "",
-        csrfToken: request.csrfToken()
-    });
+
+exports.get_estado_cuenta = async (request, response, next) => {
+    try {
+        const estudianteProfesional = await EstudianteProfesional.fetchOne(request.session.username); 
+        console.log('hola', estudianteProfesional)
+        const matricula = request.session.username;
+
+        response.render('estadocuenta/estado_cuenta', {
+            username: request.session.username || '',
+            permisos: request.session.permisos || [],
+            csrfToken: request.csrfToken(),
+            estudianteProfesional: estudianteProfesional[0][0]
+        });
+    } catch (error) {
+        response.status(500).send("Error en la obtención del estado de cuenta: " + error);
+    }
 }
+
