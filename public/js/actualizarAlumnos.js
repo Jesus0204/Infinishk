a81f0f969ad83b28b703fcea55dbc32345b7b936
Jesus0204 <a01705442@tec.mx>
2024-04-23 12:31:21 -0600

    Empezar validacion de inputs con el for

--- public/js/actualizarAlumnos.js
+++ public/js/actualizarAlumnos.js
@@ -30,3 +30,40 @@ document.querySelectorAll('.form-enviar-datos').forEach((form, index) => {
             });
     });
 });
+
+const usuarios_length = document.getElementById('usuarios_length');
+
+for (let count = 0; count < usuarios_length.innerHTML; count++){
+
+    // Accedes a cada input y al boton con el id del counter
+    const btn_Subir = document.querySelector('#btn_Subir' + count);
+    const referencia = document.querySelector('#referencia' + count);
+    const beca = document.querySelector('#beca' + count);
+
+    //  // Checar si hay contenido dentro del input, pata desactivar el boton
+    //  function checar_contenido() {
+    //      btn_Subir.disabled = nombre.value.length === 0;
+    //  }
+
+    //  // Activar mensaje si el motivo no tiene input
+    //  function mensaje_nombre() {
+    //      if (nombre.value.length === 0) {
+    //          ayuda_nombre.classList.remove('is-hidden');
+    //      } else {
+    //          ayuda_nombre.classList.add('is-hidden');
+    //      }
+    //  }
+
+    //  function mensaje_monto() {
+
+    //      if (monto.value.includes('e') || monto.value.includes('E')) {
+    //          btn_Subir.disabled = true;
+    //          ayuda_monto_exponente.classList.remove('is-hidden');
+    //      } else {
+    //          ayuda_monto_exponente.classList.add('is-hidden');
+    //      }
+    //  }
+
+     referencia.addEventListener('input', checar_contenido);
+     referencia.addEventListener('input', mensaje_nombre);
+};
