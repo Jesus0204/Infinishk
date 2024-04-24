5a72722075790bbdeedbb77f07c0cbfbadb32f95
Mikebarrsan <A01710304@tec.mx>
2024-04-23 19:37:15 -0600

    Solo registrar usuarios que no sean alumnos

--- models/rol.model.js
+++ models/rol.model.js
@@ -6,6 +6,6 @@ module.exports = class rol {
     }
 
     static fetchAll() {
-        return db.execute(`SELECT nombreRol FROM Rol WHERE 1`);
+        return db.execute(`SELECT nombreRol FROM Rol WHERE IDRol!=3`);
     }
 };
