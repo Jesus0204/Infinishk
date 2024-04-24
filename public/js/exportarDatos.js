60e69721642aaf6bbd28bb89c8ff7f9b96201ab8
Jesus0204 <a01705442@tec.mx>
2024-04-23 23:26:28 -0600

    Agregar validación de input

--- public/js/exportarDatos.js
+++ public/js/exportarDatos.js
@@ -8,3 +8,60 @@ const calendars = bulmaCalendar.attach('[type="date"]', {
     lang: 'es',
     showFooter: false
 });
+
+const btn_exportar = document.querySelector('#btn_exportar');
+
+function clear_button_1() {
+    const ayuda_fecha = document.getElementById('ayuda_fecha_vacia_1');
+    ayuda_fecha.classList.remove('is-hidden');
+    btn_exportar.disabled = true;
+}
+
+function clear_button_2() {
+    const ayuda_fecha = document.getElementById('ayuda_fecha_vacia_2');
+    ayuda_fecha.classList.remove('is-hidden');
+    btn_exportar.disabled = true;
+}
+
+function activate_clear_button_1() {
+    const ayuda_fecha = document.getElementById('ayuda_fecha_vacia_1');
+    ayuda_fecha.classList.add('is-hidden');
+}
+
+function activate_clear_button_2() {
+    const ayuda_fecha = document.getElementById('ayuda_fecha_vacia_2');
+    ayuda_fecha.classList.add('is-hidden');
+}
+
+const fecha_fin = document.querySelector('#fecha_fin');
+const fecha_inicio = document.querySelector('#fecha_inicio');
+
+function activate_button(){
+    if (fecha_fin.value.length != 0 && fecha_inicio.value.length != 0) {
+        btn_exportar.disabled = false;
+    }
+}
+
+// Sacar los botones de clear para agregar validaciones
+const clear = document.querySelectorAll('.datetimepicker-clear-button');
+let count = 0;
+clear.forEach((button) => {
+    if (count == 0) {
+        button.addEventListener('click', clear_button_1)
+    } else if (count == 1) {
+        button.addEventListener('click', clear_button_2)
+    }
+    count++;
+})
+
+let count_activate = 0;
+calendars.forEach((calendar) => {
+    if (count_activate == 0) {
+        calendar.on('select', activate_clear_button_1);
+        calendar.on('hide', activate_button);
+    } else if (count_activate == 1) {
+        calendar.on('select', activate_clear_button_2);
+        calendar.on('hide', activate_button);
+    }
+    count_activate++;
+})
