39459ee38a8489aade85c7309a4aa5d1869df713
Jesus0204 <a01705442@tec.mx>
2024-04-27 15:19:07 -0600

    Prueba para ver si lee el pago completado

--- controllers/pagos.controller.js
+++ controllers/pagos.controller.js
@@ -684,12 +684,10 @@ exports.post_subir_archivo = (request, response, next) => {
                     const fechaParseada = new Date(pagoCompleto[0][0].fechaPago)
                     const fechaFormateada = moment(fechaParseada).format('YYYY-MM-DD HH:mm');
 
-                    console.log('La fecha es: ,' + fila.fechaFormato + ',');
-
                     const montoRedondeado = Math.round(pagoCompleto[0][0].montoPagado * 100) / 100;
                     const importeRedondeado = Math.round(fila.Importe * 100) / 100;
 
-                    if (montoRedondeado === importeRedondeado && fechaFormateada === fila.fechaFormato) {
+                    if (montoRedondeado === importeRedondeado && fechaFormateada == fila.fechaFormato) {
                         tipoPago = 'Pago Completo';
                         deudaEstudiante = 0;
                     }
