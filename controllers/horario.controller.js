0bb731ca4c5ac9ae8e60056f9381208dfaaff86f
barronsguillermo <A01207945@itesm.mx>
2024-04-26 20:03:57 -0600

    Solamente consultar horario listo

--- controllers/horario.controller.js
+++ controllers/horario.controller.js
@@ -4,10 +4,9 @@ const Periodo = require('../models/periodo.model');
 
 exports.get_propuesta_horario = async (request, response, next) => {
     const schedule = await Grupo.fetchSchedule(request.session.username)
-    console.log(schedule)
     const precioTotal = await Grupo.fetchPrecioTotal(request.session.username)
-    console.log(precioTotal)
     const confirmacion = await Alumno.fetchHorarioConfirmado(request.session.username)
+    console.log(schedule)
     response.render('alumnos/consultarHorario', {
         schedule: schedule,
         precioTotal: precioTotal,
