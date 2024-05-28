6d51e0eb68f538072d26442b4fcddca24f7ee3c7
Jesus0204 <a01705442@tec.mx>
2024-05-28 08:49:34 -0600

    Navbar mejoras

--- public/js/head.js
+++ public/js/head.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 });
 
 const click_target = (element) => {
-    if (window.innerWidth < 1128) {
+    if (window.innerWidth < 1088) {
         // Get the target from the "data-target" attribute
         const target = element.dataset.target;
         const $target = document.getElementById(target);
@@ -80,16 +80,18 @@ function navbar_desktop() {
     }
 }
 
-if (window.matchMedia("(max-width: 1127px)").matches) {
+if (window.matchMedia("(max-width: 1087px)").matches) {
     navbar_movil();
 };
 
 window.addEventListener('resize', () => {
     const configuracion = document.getElementById('configuracion');
     if (configuracion) {
-        if (window.innerWidth >= 1128) {
+        if (window.innerWidth >= 1088) {
+            configuracion.style.width = '115%';
             navbar_desktop();
         } else {
+            configuracion.style.width = '100%';
             navbar_movil();
         }
     }
