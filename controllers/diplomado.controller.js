1a9a8011ad2f699ef1189dd2ba0c152f8a6a1551
Zathiald <A01705403@tec.mx>
2024-05-29 22:11:04 -0600

    Arreglar error de fechas

--- controllers/diplomado.controller.js
+++ controllers/diplomado.controller.js
@@ -124,8 +124,8 @@ exports.post_modificar_diplomado = (request, response, next) => {
     const fechaInicio_utc = fechaInicio_temp.replace(/\s/g, '');
     const fechaFin_utc = fechaFin_temp.replace(/\s/g, '');
 
-    const fechaInicio = moment(fechaInicio_utc, 'YYYY-MM-DD').add(6, 'hours').format('YYYY-MM-DD');
-    const fechaFin = moment(fechaFin_utc, 'YYYY-MM-DD').add(29, 'hours').add(59, 'minutes').add(59, 'seconds').format('YYYY-MM-DD');
+    const fechaInicio = moment(fechaInicio_utc, 'DD MM YYYY').add(6, 'hours').format('YYYY-MM-DD');
+    const fechaFin = moment(fechaFin_utc, 'DD MM YYYY').add(29, 'hours').add(59, 'minutes').add(59, 'seconds').format('YYYY-MM-DD');
 
     Diplomado.update(id, fechaInicio,fechaFin, precio, nombre)
         .then(() => {
@@ -163,8 +163,8 @@ exports.post_registrar_diplomado = (request, response, next) => {
     const fechaInicio_utc = fechaInicio_temp.replace(/\s/g, '');
     const fechaFin_utc = fechaFin_temp.replace(/\s/g, '');
 
-    const fechaInicio = moment(fechaInicio_utc, 'YYYY-MM-DD').add(6, 'hours').format('YYYY-MM-DD');
-    const fechaFin = moment(fechaFin_utc, 'YYYY-MM-DD').add(29, 'hours').add(59, 'minutes').add(59, 'seconds').format('YYYY-MM-DD');
+    const fechaInicio = moment(fechaInicio_utc, 'DD MM YYYY').add(6, 'hours').format('YYYY-MM-DD');
+    const fechaFin = moment(fechaFin_utc, 'DD MM YYYY').add(29, 'hours').add(59, 'minutes').add(59, 'seconds').format('YYYY-MM-DD');
     
     Diplomado.save(fechaInicio,fechaFin, precio, nombre)
         .then(() => {
