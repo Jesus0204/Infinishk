41eff8bd3638d8aeb3a416de7a114f74a83a8815
Jesus0204 <a01705442@tec.mx>
2024-06-15 18:03:35 -0600

    Nuevo diseño estado cuenta

--- public/js/estado_cuenta.js
+++ public/js/estado_cuenta.js
@@ -2,13 +2,16 @@ function deuda() {
     // Cambias las bars que estan activas
     const tab_deuda = document.querySelector('#nav_deuda');
     const tab_pagos = document.querySelector('#nav_pagos');
-    const tab_extras = document.querySelector('#nav_extras');
+    const tab_solicitudes = document.querySelector('#nav_solicitudes');
+    const tab_pagosExtra = document.querySelector('#nav_pagosExtra');
 
     if (tab_deuda){
         tab_deuda.classList.add('is-active');
     }
+
     tab_pagos.classList.remove('is-active');
-    tab_extras.classList.remove('is-active');
+    tab_solicitudes.classList.remove('is-active');
+    tab_pagosExtra.classList.remove('is-active');
 
     // Quitar la tabla de pagos
     const tabla_pagos = document.querySelector('#pagos');
@@ -17,6 +20,9 @@ function deuda() {
     // Quitar la tabla de extras
     const tabla_extras = document.querySelector('#extras');
     tabla_extras.classList.add('is-hidden');
+
+    const pagosExtra = document.querySelector('#pagosExtra');
+    pagosExtra.classList.add('is-hidden');
 
     // Poner la tabla de deuda
     const tabla_deuda = document.querySelector('#deuda');
@@ -30,13 +36,16 @@ function pagos() {
     // Cambias las bars que estan activas
     const tab_deuda = document.querySelector('#nav_deuda');
     const tab_pagos = document.querySelector('#nav_pagos');
-    const tab_extras = document.querySelector('#nav_extras');
+    const tab_solicitudes = document.querySelector('#nav_solicitudes');
+    const tab_pagosExtra = document.querySelector('#nav_pagosExtra');
 
     if (tab_deuda){
         tab_deuda.classList.remove('is-active');
     }
-    tab_extras.classList.remove('is-active');
+
+    tab_solicitudes.classList.remove('is-active');
     tab_pagos.classList.add('is-active');
+    tab_pagosExtra.classList.remove('is-active');
 
     // Quitar la tabla con deudas
     const tabla_deuda = document.querySelector('#deuda');
@@ -49,6 +58,9 @@ function pagos() {
     const tabla_extras = document.querySelector('#extras');
     tabla_extras.classList.add('is-hidden');
 
+    const pagosExtra = document.querySelector('#pagosExtra');
+    pagosExtra.classList.add('is-hidden');
+
     // Poner la tabla de pagos
     const tabla_pagos = document.querySelector('#pagos');
     tabla_pagos.classList.remove('is-hidden');
@@ -58,13 +70,16 @@ function extras() {
     // Cambias las bars que estan activas
     const tab_deuda = document.querySelector('#nav_deuda');
     const tab_pagos = document.querySelector('#nav_pagos');
-    const tab_extras = document.querySelector('#nav_extras');
+    const tab_solicitudes = document.querySelector('#nav_solicitudes');
+    const tab_pagosExtra = document.querySelector('#nav_pagosExtra');
 
     if (tab_deuda){
         tab_deuda.classList.remove('is-active');
     }
+
     tab_pagos.classList.remove('is-active');
-    tab_extras.classList.add('is-active');
+    tab_solicitudes.classList.add('is-active');
+    tab_pagosExtra.classList.remove('is-active');
 
     // Quitar la tabla de pagos
     const tabla_pagos = document.querySelector('#pagos');
@@ -80,42 +95,41 @@ function extras() {
     // Poner la tabla de extras
     const tabla_extras = document.querySelector('#extras');
     tabla_extras.classList.remove('is-hidden');
+
+    const pagosExtra = document.querySelector('#pagosExtra');
+    pagosExtra.classList.add('is-hidden');
 }
 
-function pagoscol() {
-    const tab_pagoscol = document.querySelector('#nav_pagoscol');
-    const tab_pagosextra = document.querySelector('#nav_pagosextra');
-   
-    tab_pagosextra.classList.remove('is-active');
-    tab_pagoscol.classList.add('is-active');
+function pagosExtra() {
+    // Cambias las bars que estan activas
+    const tab_deuda = document.querySelector('#nav_deuda');
+    const tab_pagos = document.querySelector('#nav_pagos');
+    const tab_solicitudes = document.querySelector('#nav_solicitudes');
+    const tab_pagosExtra = document.querySelector('#nav_pagosExtra');
 
-    // Quitar la tabla de extras
-    const tabla_extras = document.querySelector('#pagosextra');
+    if (tab_deuda) {
+        tab_deuda.classList.remove('is-active');
+    }
+
+    tab_pagos.classList.remove('is-active');
+    tab_solicitudes.classList.remove('is-active');
+    tab_pagosExtra.classList.add('is-active');
+
+    // Quitar la tabla de pagos
+    const tabla_pagos = document.querySelector('#pagos');
+    tabla_pagos.classList.add('is-hidden');
+
+    // Quitar la tabla de deudas
+    const tabla_deuda = document.querySelector('#deuda');
+
+    if (tabla_deuda) {
+        tabla_deuda.classList.add('is-hidden');
+    }
+
+    // Poner la tabla de extras
+    const tabla_extras = document.querySelector('#extras');
     tabla_extras.classList.add('is-hidden');
 
-    // Poner la tabla de pagos colegiatura
-    const tabla_pagoscol = document.querySelector('#pagoscol');
-    tabla_pagoscol.classList.remove('is-hidden');
-}
-
-function pagosextra() {
-    const tab_pagoscol = document.querySelector('#nav_pagoscol');
-    const tab_pagosextra = document.querySelector('#nav_pagosextra');
-   
-    tab_pagosextra.classList.add('is-active');
-    tab_pagoscol.classList.remove('is-active');
-
-    // Quitar la tabla de pagos colegiatura
-    const tabla_pagoscol = document.querySelector('#pagoscol');
-    tabla_pagoscol.classList.add('is-hidden');
-
-    // Poner la tabla de pagos extras
-    const tabla_extras = document.querySelector('#pagosextra');
-    tabla_extras.classList.remove('is-hidden');
-
-    // Quitar la tabla de pagos colegiatura dip
-    const tabla_pagosdip = document.querySelector('#pagosdip');
-    tabla_pagosdip.classList.add('is-hidden');
-
-    
+    const pagosExtra = document.querySelector('#pagosExtra');
+    pagosExtra.classList.remove('is-hidden');
 }
