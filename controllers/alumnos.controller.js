2b4fb765d241b9f1a80b98877a507548f81be9c4
Zathiald <A01705403@tec.mx>
2024-06-27 15:35:01 -0600

    Arreglar funcion para dar de baja

--- controllers/alumnos.controller.js
+++ controllers/alumnos.controller.js
@@ -77,19 +77,17 @@ exports.get_datos = async (request, response, next) => {
 exports.post_dar_baja_grupo = async (request, response, next) => {
     const { IDGrupo, matricula } = request.body;
 
-    // Formateas la fecha y hora en el formato deseado
-    const fecha_actual = moment().tz('America/Mexico_City');
-    const fecha_formateada = fecha_actual.format('YYYY-MM-DD');
-
     try {
         const resultfetchCreditoActivo = await PrecioCredito.fetchCreditoActivo(); // Espera a que la promesa se resuelva
         const resultfetchIDPorGrupo = await Materia.fetchIDPorGrupo(IDGrupo);
         const resultfetchBeca = await Alumno.fetchBeca(matricula);
+        const resultfetchCredito = await Alumno.fetchCreditoINT(matricula);
         const creditoactual = resultfetchCreditoActivo[0][0].precioPesos; // Acceder al valor numérico
         const IDMateria = resultfetchIDPorGrupo[0][0].IDMateria;
         const Beca = resultfetchBeca[0][0].beca;
-
-        await Fichas.delete_grupo_update_fichas(matricula, IDGrupo, creditoactual, IDMateria, Beca);
+        const Credito = resultfetchCredito[0][0].credito;
+        
+        await Fichas.delete_grupo_update_fichas(matricula, IDGrupo, creditoactual, IDMateria, Beca, Credito);
 
         // Función Modificar fichas de pago
         // Función Eliminar Grupo
