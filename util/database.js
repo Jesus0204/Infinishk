0772cd00f20292514c1ec78e3b2d19dad99a6df4
Jesus0204 <a01705442@tec.mx>
2025-01-09 09:27:21 -0600

    chore: preparar BD para deploy

--- util/database.js
+++ util/database.js
@@ -1,10 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'via_pago',
-    password: ''
-});
+const pool = mysql.createPool(process.env.JAWSDB_MARIA_URL);
 
 module.exports = pool.promise();
