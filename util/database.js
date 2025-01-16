0fe82ca60d446cabd8bf5a1f6f5462e46c548b6f
Jesus0204 <a01705442@tec.mx>
2025-01-16 14:03:15 -0600

    feat: agregar rol en tabla de usuarios

--- util/database.js
+++ util/database.js
@@ -1,5 +1,10 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool(process.env.JAWSDB_MARIA_URL);
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'via_pago',
+    password: 'Queretaro2017#'
+});
 
 module.exports = pool.promise();
